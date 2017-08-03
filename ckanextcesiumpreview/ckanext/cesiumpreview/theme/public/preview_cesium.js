9825fa7550c5c1692bdea1d128bc3204666dda19
Matt Fullerton <matt.fullerton@gmail.com>
2017-08-03 17:04:46 +0000

    Change to proxied URL

--- ckanextcesiumpreview/ckanext/cesiumpreview/theme/public/preview_cesium.js
+++ ckanextcesiumpreview/ckanext/cesiumpreview/theme/public/preview_cesium.js
@@ -5,7 +5,7 @@ ckan.module('cesiumpreview', function (jQuery, _) {
             var self = this;
 
 //      var vis_server = 'http://localhost';  //local
-            var vis_server = 'http://172.104.153.179:3001/';
+            var vis_server = 'http://172.104.153.179/terria/';
 
             var config = {
                 "version": "0.0.03",
