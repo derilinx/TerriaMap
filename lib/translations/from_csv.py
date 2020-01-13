799221c0020501e1e2e3770958f9b4fbcfcc8e3f
gtkChop <swaroopbhatk@gmail.com>
2020-01-13 14:28:50 +0000

    language code for laos error chnaged from la to lo

--- lib/translations/from_csv.py
+++ lib/translations/from_csv.py
@@ -2,21 +2,21 @@ import csv
 import json
 import collections
 
-fields = ['notes', 'key', 'en', 'th', 'km', 'vm', 'my', 'la']
+fields = ['notes', 'key', 'en', 'th', 'km', 'vm', 'my', 'lo']
 
 translations = collections.defaultdict(dict)
 
 with open('translated-complete.csv', 'r') as f:
     reader = csv.DictReader(f , fieldnames=fields)
     # header
-    reader.next()
+    next(reader)
 
     for line in reader:
         try:
             section, key = line['key'].split('.')
-            print line['key']
-            print section, key
-            translations[section][key] = [line['en'], line['th'], line['km'], line['vm'], line['my'], line['la']]
+            print(line['key'])
+            print(section, key)
+            translations[section][key] = [line['en'], line['th'], line['km'], line['vm'], line['my'], line['lo']]
         except Exception as msg:
             #print msg
             #print line['key']
